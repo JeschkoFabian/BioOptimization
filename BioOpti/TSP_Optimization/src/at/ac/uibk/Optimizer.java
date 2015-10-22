6e7f5e44ecc63066674f81696a46a9cd1e1b745e
fabian <fabian.jeschko@uibk.ac.at>
2015-10-22 10:03:33 +0200

    optimized~

--- BioOpti/TSP_Optimization/src/at/ac/uibk/Optimizer.java
+++ BioOpti/TSP_Optimization/src/at/ac/uibk/Optimizer.java
@@ -1,45 +1,80 @@
 package at.ac.uibk;
 
+import java.util.Random;
+
 public class Optimizer {
+	private static Random r = new Random();
 
-
-	private static int[] generateRandomArray(int size){
+	private static int[] generateRandomArray(int size) {
 		int[] arr = new int[size];
-		
+
 		// arr with 0->1->2....
-		for (int i = 0; i < size; i++){
+		for (int i = 0; i < size; i++) {
 			arr[i] = (i + 1) % size;
 		}
 
 		// randomize by permutations
-		for (int i = 0; i < size; i++){
+		for (int i = 0; i < size; i++) {
 			// random rounded number
-			int toSwitch = (int) (Math.random() * size);
-			
-			int tmp = arr[i];
-			arr[i] = arr[toSwitch];
-			arr[toSwitch] = tmp;
+			int toSwitch = r.nextInt(size);
+
+			arr = swap(arr, i, toSwitch);
 		}
-		
+
 		return arr;
 	}
-	
+
 	// takes an array of nodes as input, returns the order to travel them
-	public static int[] optimize(TSP_Node[] input){
+	public static int[] optimize(TSP_Node[] input, int iter) {
 		int[] ordering = generateRandomArray(input.length);
-		
-		
+
+		double currentDist = calculatePath(ordering, input);
+		// System.out.println("Initial Path length: " + (int) currentDist);
+
+		for (int k = 0; k < iter; k++) {
+			double tmpDist = currentDist;
+
+			for (int i = 0; i < input.length - 1; i++) {
+				for (int j = i + 1; j < input.length; j++) {
+
+					ordering = swap(ordering, i, j);
+					double newDist = calculatePath(ordering, input);
+
+					if (newDist >= currentDist) {
+						// allow worse switches, the closer the new dist is to
+						// the current, the better the chances
+						if ((newDist - currentDist) / currentDist > r.nextDouble() / iter)
+							ordering = swap(ordering, i, j);
+					} else {
+						currentDist = newDist;
+					}
+				}
+			}
+
+			// cancel earlier if nothing better can be achieved
+			if (tmpDist == currentDist)
+				break;
+		}
+		// System.out.println("Optimized path length: " + (int) currentDist);
+
 		return ordering;
 	}
-	
-	public static int calculatePath(int[] order, TSP_Node[] nodes){
-		int dist = 0;
-		
-		for (int i = 0; i < order.length; i++){
-			dist += nodes[i].calculateDistance(nodes[order[i]]);
+
+	private static int[] swap(int[] arr, int x, int y) {
+		int tmp = arr[x];
+		arr[x] = arr[y];
+		arr[y] = tmp;
+
+		return arr;
+	}
+
+	public static double calculatePath(int[] order, TSP_Node[] nodes) {
+		double dist = 0;
+
+		for (int i = 0; i < order.length; i++) {
+			dist += nodes[order[i]].calculateDistance(nodes[order[(i + 1) % order.length]]);
 		}
-		
 		return dist;
 	}
-	
+
 }
--- BioOpti/TSP_Optimization/src/at/ac/uibk/TSP_Exec.java
+++ BioOpti/TSP_Optimization/src/at/ac/uibk/TSP_Exec.java
@@ -1,24 +1,55 @@
 package at.ac.uibk;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TSP_Exec {
 
 	public static void main(String[] args) {
-		int sz = 25;
+		int sz = 51;
 		TSP_Node[] nodes = new TSP_Node_Impl[sz];
 
-		for (int i = 0; i < sz; i++) {
-//			int x = (int) (Math.random() * 1000);
-//			int y = (int) (Math.random() * 1000);
-			nodes[i] = new TSP_Node_Impl(i, 0);
+		String problem = "1 37 52 " + "2 49 49 " + "3 52 64 " + "4 20 26 " + "5 40 30 " + "6 21 47 " + "7 17 63 "
+				+ "8 31 62 " + "9 52 33 " + "10 51 21 " + "11 42 41 " + "12 31 32 " + "13 5 25 " + "14 12 42 "
+				+ "15 36 16 " + "16 52 41 " + "17 27 23 " + "18 17 33 " + "19 13 13 " + "20 57 58 " + "21 62 42 "
+				+ "22 42 57 " + "23 16 57 " + "24 8 52 " + "25 7 38 " + "26 27 68 " + "27 30 48 " + "28 43 67 "
+				+ "29 58 48 " + "30 58 27 " + "31 37 69 " + "32 38 46 " + "33 46 10 " + "34 61 33 " + "35 62 63 "
+				+ "36 63 69 " + "37 32 22 " + "38 45 35 " + "39 59 15 " + "40 5 6 " + "41 10 17 " + "42 21 10 "
+				+ "43 5 64 " + "44 30 15 " + "45 39 10 " + "46 32 39 " + "47 25 32 " + "48 25 55 " + "49 48 28 "
+				+ "50 56 37 " + "51 30 40";
+		// for (int i = 0; i < sz; i++) {
+		// nodes[i] = new TSP_Node_Impl(i, 0);
+		// }
+		Scanner sc = new Scanner(problem);
+
+		while (sc.hasNextInt()) {
+			int pos = sc.nextInt();
+			nodes[pos - 1] = new TSP_Node_Impl(sc.nextInt(), sc.nextInt());
 		}
-		
-		int[] path = Optimizer.optimize(nodes);
-		int distance = Optimizer.calculatePath(path, nodes);
-		
-		System.out.println("Path distance: " +  distance);
-		System.out.println("Order: " + Arrays.toString(path));
+
+		sc.close();
+
+		double avg = 0;
+		double bestDist = Double.MAX_VALUE;
+		int[] bestOrder = new int[0];
+		int iter = 20;
+		for (int i = 0; i < iter; i++) {
+			int[] tmpOrder = Optimizer.optimize(nodes, 100);
+
+			double tmpDist = Optimizer.calculatePath(tmpOrder, nodes);
+			avg += tmpDist;
+			
+			if (tmpDist < bestDist){
+				bestDist = tmpDist;
+				bestOrder = tmpOrder;
+			}
+		}
+
+		System.out.println("Average Dist: " + (int) (avg / iter));
+		System.out.println("Best Dist: " + (int) bestDist);
+		System.out.println("Best Order: " + Arrays.toString(bestOrder));
+
+		System.out.println("Best known for eil51: 426");
 	}
 
 }
