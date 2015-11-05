08746212043cbf98a50dcc06b2e03da32d370632
fabian <fabian.jeschko@uibk.ac.at>
2015-11-05 11:52:04 +0100

    General construct and minimal method implementation,
    eval + generate init + recomb implemented (somewhat)

--- Genetic_Sudoku/src/at/ac/uibk/SolverExec.java
+++ Genetic_Sudoku/src/at/ac/uibk/SolverExec.java
@@ -3,7 +3,21 @@ package at.ac.uibk;
 public class SolverExec {
 
 	public static void main(String[] args) {
+		int[][] initial = { { 0, 0, 6, 0, 8, 9, 4, 0, 7 }, { 0, 0, 0, 7, 0, 0, 6, 0 },
+				{ 0, 7, 0, 6, 0, 0, 0, 2, 0 }, { 4, 0, 8, 0, 0, 5, 0, 9, 0 }, { 7, 0, 0, 0, 0, 0, 0, 0, 1 },
+				{ 0, 9, 0, 7, 0, 0, 6, 0, 2 }, { 0, 8, 0, 0, 0, 2, 0, 5, 0 }, { 0, 4, 0, 0, 1, 0, 0, 0, 0 },
+				{ 3, 0, 2, 4, 5, 0, 9, 0, 0 } };
+
+		SudokuSolver solver = new SudokuSolver(initial);
+
+		int[][] solution = solver.solve();
 		
+		for (int i = 0; i < 9; i++){
+			for (int j = 0; j < 9; j++){
+				System.out.print(solution[i][j] + ", ");
+			}
+			System.out.println("");
+		}
 	}
 
 }
---
+++ Genetic_Sudoku/src/at/ac/uibk/SudokuSolver.java
@@ -0,0 +1,160 @@
+package at.ac.uibk;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SudokuSolver {
+	private final int MAX_POPULATION = 10;
+	private SecureRandom r = new SecureRandom();
+	private int[][] initial;
+	List<int[][]> population;
+
+	public SudokuSolver(int[][] initial) {
+		this.initial = initial;
+	}
+
+	public int[][] solve() {
+		population = generateInitialPopulation();
+
+		int[] fitPop = evaluate(population);
+
+		boolean cond = true;
+
+		while (cond) {
+			List<int[][]> tmp = recombine(fitPop, population);
+
+			tmp = mutate(tmp);
+
+			int[] fitTmp = evaluate(tmp);
+
+			population = select(population, tmp);
+
+		}
+		return initial;
+	}
+
+	private List<int[][]> generateInitialPopulation() {
+		List<int[][]> population = new ArrayList<int[][]>();
+
+		for (int i = 0; i < MAX_POPULATION; i++) {
+			int[][] tmp = new int[9][9];
+
+			for (int j = 0; j < 9; j++) {
+				for (int k = 0; k < 9; k++) {
+					if (initial[j][k] == 0) {
+						tmp[j][k] = r.nextInt(9) + 1;
+					} else {
+						tmp[j][k] = initial[j][k];
+					}
+				}
+			}
+
+			population.add(tmp);
+		}
+
+		return population;
+	};
+
+	/**
+	 * return number of contradictions in each solution
+	 * 
+	 * alternate solution: sum up all rows/columns, need to be sum(1-9), the
+	 * closer the better if all are sum(1-9) then optimal
+	 * 
+	 * @param population
+	 */
+	private int[] evaluate(List<int[][]> population) {
+		int[] contradictions = new int[population.size()];
+
+		for (int x = 0; x < population.size(); x++) {
+			int[][] entry = population.get(x);
+
+			boolean[] horizontalNums = getFalseArr();
+			boolean[][] verticalNums = new boolean[9][9];
+
+			for (int i = 0; i < 9; i++) {
+				verticalNums[i] = getFalseArr();
+			}
+
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+					// check horizontal
+					if (horizontalNums[entry[i][j]]) {
+						contradictions[x]++;
+					} else {
+						horizontalNums[entry[i][j]] = true;
+					}
+
+					// check vertical
+					if (verticalNums[i][entry[i][j]]) {
+						contradictions[x]++;
+					} else {
+						verticalNums[i][entry[i][j]] = true;
+					}
+				}
+			}
+		}
+
+		return contradictions;
+	}
+
+	private boolean[] getFalseArr() {
+		boolean[] arr = new boolean[9];
+
+		for (int i = 0; i < 9; i++) {
+			arr[i] = false;
+		}
+
+		return arr;
+	}
+
+	private List<int[][]> recombine(int[] eval, List<int[][]> population) {
+		List<int[][]> children = new ArrayList<int[][]>();
+
+		for (int i = 0; i < population.size(); i++) {
+			// Choose parents
+
+			int parent1, parent2;
+			int rand1 = r.nextInt(population.size());
+			int rand2 = r.nextInt(population.size());
+
+			if (eval[rand1] < eval[rand2]) {
+				parent1 = rand1;
+			} else {
+				parent1 = rand2;
+			}
+
+			rand1 = r.nextInt(population.size());
+			rand2 = r.nextInt(population.size());
+
+			if (eval[rand1] < eval[rand2]) {
+				parent2 = rand1;
+			} else {
+				parent2 = rand2;
+			}
+
+			// combine parents
+			// simple cut
+			int cut = r.nextInt(81);
+
+			for (int j = 0; j < 9; j++) {
+				for (int k = 0; k < 9; k++) {
+
+				}
+			}
+
+		}
+
+		return children;
+	}
+
+	private List<int[][]> mutate(List<int[][]> population) {
+		// mutate into more feasible!
+		return population;
+	}
+
+	private List<int[][]> select(List<int[][]> initial, List<int[][]> evolved) {
+		return initial;
+	}
+}
