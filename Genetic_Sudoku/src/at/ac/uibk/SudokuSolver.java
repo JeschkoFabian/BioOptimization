316782fbe67cddbe046bc5ad3e0ceb6181a75d17
Dani <Dani@Dani>
2015-11-07 00:50:55 +0100

    A few additions (mutate, crossover) and small bugfixes (mostly array overflow problems)

--- Genetic_Sudoku/src/at/ac/uibk/SudokuSolver.java
+++ Genetic_Sudoku/src/at/ac/uibk/SudokuSolver.java
@@ -3,7 +3,9 @@ package at.ac.uibk;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SudokuSolver {
 	private final int MAX_POPULATION = 10;
@@ -16,13 +18,15 @@ public class SudokuSolver {
 	}
 
 	public int[][] solve() {
-		population = generateRandomInitialPopulation();
+		population = generateInitialPopulation();
 
 		int[] fitPop = evaluate(population);
 
-		boolean cond = true;
-
-		while (cond) {
+		//boolean cond = true;
+		int i = 0;
+		int fittest = Integer.MAX_VALUE;
+		int [][] solution = new int[9][9];
+		while (i<10000) {
 			List<int[][]> tmp = recombine(fitPop, population);
 
 			tmp = mutate(tmp);
@@ -30,9 +34,18 @@ public class SudokuSolver {
 			int[] fitTmp = evaluate(tmp);
 
 			population = select(population, tmp);
-
+			
+			//get the current fittest solution
+			for (int x =0; x<10;x++) {
+				if (fitTmp[x] < fittest) {
+					fittest = fitTmp[x];
+					solution = population.get(x);
+				}
+			}
+			i++;
 		}
-		return initial;
+		//System.out.println(fittest);
+		return solution;
 	}
 
 	private List<int[][]> generateRandomInitialPopulation() {
@@ -68,11 +81,16 @@ public class SudokuSolver {
 
 		List<int[][]> population = new ArrayList<int[][]>();
 
+		for (int i = 0; i < 9; i++) {
+		stack[i] = 9;
+		}
+		
 		// remove initial elements from the stack
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
 				if (initial[i][j] != 0) {
-					stack[initial[i][j]]--;
+					//fixed some index problems - please double check!
+					stack[initial[i][j]-1]--;
 				}
 			}
 		}
@@ -89,10 +107,10 @@ public class SudokuSolver {
 						while (stack[tmp] < 1) {
 							tmp = r.nextInt(9);
 						}
-
 						tmpStack[tmp]--;
-						tmpSudoku[j][k] = tmp;
+						tmpSudoku[j][k] = tmp+1;
 					}
+				
 				}
 			}
 
@@ -133,20 +151,21 @@ public class SudokuSolver {
 				verticalNums[i] = getFalseArr();
 			}
 
+			//fixed some more index problems
 			for (int i = 0; i < 9; i++) {
 				for (int j = 0; j < 9; j++) {
 					// check horizontal
-					if (horizontalNums[entry[i][j]]) {
+					if (horizontalNums[entry[i][j] - 1]) {
 						contradictions[x]++;
 					} else {
-						horizontalNums[entry[i][j]] = true;
+						horizontalNums[entry[i][j] - 1] = true;
 					}
 
 					// check vertical
-					if (verticalNums[i][entry[i][j]]) {
+					if (verticalNums[i][entry[i][j] - 1]) {
 						contradictions[x]++;
 					} else {
-						verticalNums[i][entry[i][j]] = true;
+						verticalNums[i][entry[i][j] - 1] = true;
 					}
 				}
 			}
@@ -193,12 +212,19 @@ public class SudokuSolver {
 			// combine parents
 			// simple cut
 			int cut = r.nextInt(81);
-
+			int[][] temp = new int[9][9];
+			int[][] par1 = population.get(parent1);
+			int[][] par2 = population.get(parent2);
 			for (int j = 0; j < 9; j++) {
 				for (int k = 0; k < 9; k++) {
-
+					if ((j * 9 + k) < cut) {
+						temp[j][k] = par1[j][k];
+					} else {
+						temp[j][k] = par2[j][k];
+					}
 				}
 			}
+			children.add(temp);
 
 		}
 
@@ -207,7 +233,40 @@ public class SudokuSolver {
 
 	private List<int[][]> mutate(List<int[][]> population) {
 		// mutate into more feasible!
-		return population;
+		List<int[][]> mutated = new ArrayList<int[][]>();
+		for (int[][] x : population) {
+			//Count the amount of each number
+			Map<Integer, Integer> countMap = new HashMap<Integer, Integer>();
+			for (int i = 0; i < 9; i++) {
+				countMap.put(i, 0);
+			}
+			int[][] temp = new int[9][9];
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+					countMap.put(x[i][j]-1, countMap.get(x[i][j]-1) + 1);
+				}
+			}
+
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+
+					if (initial[i][j] == 0) {
+						temp[i][j] = x[i][j];
+					} else {
+						//based on the amount of each number, the probability for mutation is slightly varied
+						//e.g. #8 is 12 times in the current sudoku -> prob. for mutation goes from 1/81 to (12/9)/81
+						//still much work to be done here...
+						if (r.nextInt(81) < countMap.get(x[i][j]-1) / 9) {
+							temp[i][j] = r.nextInt(9) + 1;
+						} else {
+							temp[i][j] = x[i][j];
+						}
+					}
+				}
+			}
+			mutated.add(temp);
+		}
+		return mutated;
 	}
 
 	private List<int[][]> select(List<int[][]> initial, List<int[][]> evolved) {
